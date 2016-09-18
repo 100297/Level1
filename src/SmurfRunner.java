4d00491910e55e0f5f7efea4f529b78755894f4b
Goldeenzu <league@WTS10.attlocal.net>
2016-09-18 11:27:38 -0700

    stuf
    sad

---
+++ src/SmurfRunner.java
@@ -0,0 +1,23 @@
+
+public class SmurfRunner {
+public static void main(String[] args) {
+	Smurf HandySmurf = new Smurf("HandySmurf");
+	HandySmurf.eat();
+	HandySmurf.getName();
+	HandySmurf.getHatColor("red");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
